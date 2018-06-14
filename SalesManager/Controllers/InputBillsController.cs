18f88e6395d41525e754d9e111a1ad6919c050ad
hieuhan <hantrunghieu@gmail.com>
2018-06-14 12:02:00 +0700

    ...
    ...

--- SalesManager/Controllers/InputBillsController.cs
+++ SalesManager/Controllers/InputBillsController.cs
@@ -14,7 +14,7 @@ namespace SalesManager.Controllers
     public class InputBillsController : Controller
     {
         // GET: InputBills
-        public ActionResult Index(int customerId = 0, int userId = 0, int page = 1, byte billStatusId = 0, byte paymentTypeId = 0, byte warehouseId = 0, string dateFrom = "", string dateTo = "")
+        public ActionResult Index(int customerId = 0, int userId = 0, int supplierId = 0, int page = 1, byte billStatusId = 0, byte paymentTypeId = 0, byte warehouseId = 0, string dateFrom = "", string dateTo = "")
         {
             int rowCount = 0;
             var model = new InputBillsModel
@@ -24,7 +24,7 @@ namespace SalesManager.Controllers
                 ListWarehouse = new Warehouse().GetAll(),
                 ListSuppliers = new Suppliers().GetAll(),
                 ListUsers = new Users().GetAll(),
-                ListInputBills = new InputBills { CustomerId = customerId, UserId = userId, BillStatusId = billStatusId, PaymentTypeId = paymentTypeId , WarehouseId = warehouseId }.GetPage(dateFrom, dateTo, string.Empty, SalesManagerConstants.RowAmount20, page > 0 ? page - 1 : page, ref rowCount),
+                ListInputBills = new InputBills { CustomerId = customerId, UserId = userId, SupplierId = supplierId, BillStatusId = billStatusId, PaymentTypeId = paymentTypeId , WarehouseId = warehouseId }.GetPage(dateFrom, dateTo, string.Empty, SalesManagerConstants.RowAmount20, page > 0 ? page - 1 : page, ref rowCount),
                 RowCount = rowCount,
                 Pagination = new PaginationModel
                 {
